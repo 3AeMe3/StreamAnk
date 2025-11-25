10786074c81286c2a44aa7ca57865db0c6712952
Frank <fm792086@gmail.com>
2025-11-25 16:16:31 -0500

    add loading animation

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,7 +1,24 @@
-export default function Loading(){
-    return(
-        <div className="bg-black h-screen flex justify-center items-center">
-            <h1>CARGANDO</h1>
+export default function Loading() {
+  return (
+    <>
+      <div className="loader">
+        <span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div className="base">
+          <span></span>
+          <div className="face"></div>
         </div>
-    )
+      </div>
+      <div className="longfazers">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </>
+  );
 }
